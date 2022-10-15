8d4eb879d4f5f4ad3976ad497f1718ead11548a3
triggerX <moudourousamuel@gmail.com>
2022-10-15 10:49:46 +0100

    completed app

---
+++ src/components/DropZone.vue
@@ -0,0 +1,77 @@
+<template>
+    <div 
+    @dragenter.prevent="toggleActive"
+    @dragleave.prevent="toggleActive"
+    @dragover.prevent
+    @drop.prevent="toggleActive"
+
+    :class="{'active-dropzone' : active }"
+    class="dropzone">
+        <span>Drag or Drop File</span>
+        <span>OR</span>
+        <label for="dropzoneFile">Select File</label>
+        <input type="file"  id="dropzoneFile" class="dropzoneFile">
+    </div>
+</template>
+
+
+
+<script setup>
+import { ref } from "vue";
+
+
+const active = ref(false);
+
+// passage de la classe "dropzone" en mode active-desactive
+
+const toggleActive = () => {
+    active.value = !active.value;
+};
+
+
+
+</script>
+
+
+
+<style scoped>
+.dropzone{
+    width: 600px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(241, 255, 250);
+    border-radius: 10px;
+    gap: 16px;
+    border: 2px dashed aquamarine;
+    transition: 0.3s ease-in-out all;
+
+
+    }
+.dropzone > label{
+        padding: 8px 12px;
+        color: #fff;
+        background-color: rgb(0, 224, 150);
+        transition: 0.3s ease-in-out all;
+        border-radius: 5px;
+    }
+
+.dropzone > input{
+    display: none;
+}
+
+.active-dropzone{
+    background-color:  rgba(0, 44, 29, 0.603);
+    border: 2px dashed rgb(1, 247, 165);
+    color: #fff;
+
+}
+
+.active-dropzone > label{
+    color: rgb(0, 194, 129);
+    background-color: rgb(255, 255, 255);
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<script setup>
+import DropZone from '../components/DropZone.vue';
+import { ref } from "vue";
+
+let dropzoneFile = ref("");
+
+// choisir le fichier par drag & drop
+
+const drop = (e) => {
+  dropzoneFile.value = e.dataTransfer.files[0];
+};
+
+// choisir le fichier en cliquant sur le bouton "selectfile"
+
+const selectedFile = () => {
+  dropzoneFile.value = document.querySelector(".dropzoneFile").files[0];
+};
+</script>
+
+<template>
+  <div class="home">
+    <h1>Drop your file here</h1>
+    <DropZone @drop.prevent="drop" @change="selectedFile"/>
+    <span class="file-info">File: {{dropzoneFile.name}}</span>
+  </div>
+</template>
+
+
+<style>
+.home{
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 32px;
+}
+</style>
